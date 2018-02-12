a99b3fec18b69ba6195a2cff859dd4ff058b08b6
SchulerSimon <36367814+SchulerSimon@users.noreply.github.com>
2018-02-12 13:04:54 +0100

    Added Adler-32 Inplementation in Java
    This is just for fun and by FAR from Optimal.

---
+++ Adler32.java
@@ -0,0 +1,29 @@
+/**
+ *@Author Simon Schuler 
+ *
+ *This Class implements the Adler-32 Hash-Function in java.
+ *This is FAR from optimal, I was just using it to generate
+ *some hashes for some of my Students.
+ */
+public class Adler32 {
+	private static final int MOD_ADLER = 65521;
+	public static void main(String[] args) {
+		String input = "";
+		//append all the args together with spaces.
+		for(int i = 0; i < args.length; i++) {
+			if ( i < args.length - 1) {
+				input += args[i] + " ";
+			} else {
+				input += args[i];
+			}
+		}
+		//this is where the matic happens.
+		int a=1, b=0;
+		for(int i = 0; i < input.length(); i++) {
+			a = (a + input.charAt(i)) % MOD_ADLER;
+			b = (b + a) % MOD_ADLER;
+		}
+
+	System.out.println(Integer.toHexString((b << 16) | a).toUpperCase());
+	}
+}
